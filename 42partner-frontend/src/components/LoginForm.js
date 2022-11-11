7515a2167a6c6c873e900bc57af0ca174e248bad
a <a>
2022-11-11 18:09:39 +0900

    feature: Add login with 42OAuth

--- 42partner-frontend/src/components/LoginForm.js
+++ 42partner-frontend/src/components/LoginForm.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import '../styles/LoginForm.scss';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 const LoginForm = () => {
+  const requestLogin = () => {
+    window.location.href = process.env.REACT_APP_OAUTH_LOGIN_URL;
+  };
+
   return (
     <div className="login-form">
       <div className="image-wrapper">
         <img alt="meal_icon" src="/images/meal_icon.png" />
         <img alt="study_icon" src="/images/study_icon.jpeg" />
       </div>
-      <Button sx={{ m: 1, width: '220px' }} variant="contained">
+      <Button
+        sx={{ m: 1, width: '220px' }}
+        variant="contained"
+        onClick={requestLogin}
+      >
         Sign in with 42 Intra
       </Button>
     </div>
