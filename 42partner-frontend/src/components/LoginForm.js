bc028b62bc8e64b010fbb4efe7fa1e7251a94bd7
luckylooky2 <luckylooky2@naver.com>
2023-01-04 21:00:21 +0900

    fix: rollback login tag

--- 42partner-frontend/src/components/LoginForm.js
+++ 42partner-frontend/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import '../styles/LoginForm.scss';
@@ -8,10 +8,10 @@ const LoginForm = () => {
   // axios로 교체
   // 302 redirection referer: "42 authorization server"
   // 302로 오는 promise를 예외 처리하여 href로 보내는 방법?
-  // const requestLogin = () => {
-  //   window.location.href =
-  //     'https://api.42partner.com/oauth2/authorization/authclient';
-  // };
+  const requestLogin = () => {
+    window.location.href =
+      'https://api.42partner.com/oauth2/authorization/authclient';
+  };
 
   return (
     <div className="login-form">
@@ -19,8 +19,8 @@ const LoginForm = () => {
         <RestaurantIcon className="icon" />
         <MenuBookIcon className="icon" />
       </div>
-      <a
-        href="https://api.42partner.com/oauth2/authorization/authclient"
+      <Button
+        onClick={requestLogin}
         style={{
           color: 'white',
           backgroundColor: 'lightPink',
@@ -30,7 +30,7 @@ const LoginForm = () => {
         }}
       >
         Login with 42Intra
-      </a>
+      </Button>
     </div>
   );
 };
