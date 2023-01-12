d90f6aa61aa71c99c8ffaf3a98555e0f2316ab53
luckylooky2 <luckylooky2@naver.com>
2023-01-12 16:05:20 +0900

    feature: global FLAG state using useState

--- 42partner-frontend/src/App.js
+++ 42partner-frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import LoginPage from './pages/LoginPage';
 import MealRoomPage from './pages/MealRoomPage';
 import StudyRoomPage from './pages/StudyRoomPage';
@@ -10,11 +11,73 @@ import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import MealRandomPage from './pages/MealRandomPage';
 import StudyRandomPage from './pages/StudyRandomPage';
+import instance from './api/api';
 
 import './App.scss';
 import './styles/Layout.scss';
 
 const App = () => {
+  const [flag, setFlag] = useState(0);
+  const refreshToken = async (error) => {
+    const refresh = axios.create({
+      baseURL: 'https://api.42partner.com',
+      withCredentials: true,
+    });
+
+    const retry = (errorConfig) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          if (errorConfig.method === 'get')
+            resolve(instance.get(errorConfig.url));
+          else if (errorConfig.method === 'post')
+            resolve(instance.post(errorConfig.url));
+        }, 500);
+      });
+    };
+
+    let response;
+
+    try {
+      response = await refresh.post(`api/token/refresh`);
+    } catch (e) {
+      alert(e);
+    }
+
+    const newAccessToken = response.data.accessToken;
+
+    /* eslint-disable dot-notation */
+    instance.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${newAccessToken}`;
+    localStorage.setItem('accessToken', newAccessToken);
+    setFlag(flag + 1);
+    /* eslint-disable no-param-reassign */
+    error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+    error.config.headers.withCredentials = true;
+    retry(error.config);
+    return Promise.reject(error);
+  };
+
+  instance.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (error) => {
+      if (error.response.status === 401) {
+        window.location.href = '/login';
+      } else if (
+        error.response.status === 403 &&
+        error.response.data.code === 'AU001'
+      ) {
+        return refreshToken(error);
+      } else if (error.response.status === 404) {
+        alert('로그인이 필요합니다.');
+        window.location.href = '/';
+      }
+      return Promise.reject(error);
+    },
+  );
+
   return (
     <div className="App">
       <div>
@@ -30,7 +93,7 @@ const App = () => {
             <Route path="meal/room" element={<MealRoomPage />} />
             <Route path="study/random" element={<StudyRandomPage />} />
             <Route path="study/room" element={<StudyRoomPage />} />
-            <Route path="mypage" element={<MyPage />} />
+            <Route path="mypage" element={<MyPage flag={flag} />} />
           </Routes>
         </div>
       </div>
